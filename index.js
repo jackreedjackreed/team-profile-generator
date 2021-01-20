ea042418de6bbf832b0f8e6bb918fafa6cdfa17c
J-Boogie <jackr353@gmail.com>
2021-01-20 16:42:55 -0600

    add pseudo to index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+// start app
+
+// prompt user for team information
+     // manager --> first
+        // name, employee ID, email address, office number
+    // AFTER MANAGER --> add engineer, add intern, or finish building
+        // engineer
+            // name, ID, email, github username --> then back to MENU
+        // intern
+            // name, ID, email, school --> MENU
+        // employee (?)
+
+    // FINISH TEAM --> generate HTML
+
+   
+
+// in the html
+    // click an email address and open default email program
+    // click github username and open in new tab
---
+++ tests/Employee.test.js
@@ -0,0 +1,15 @@
+// The first class is an `Employee` parent class with the following properties and methods:
+
+// * `name`
+
+// * `id`
+
+// * `email`
+
+// * `getName()`
+
+// * `getId()`
+
+// * `getEmail()`
+
+// * `getRole()`&mdash;returns `'Employee'`
