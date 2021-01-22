c511eaa9bb34c8bee124bad17e561d316a244aa4
J-Boogie <jackr353@gmail.com>
2021-01-22 14:45:42 -0600

    get jest to function properly

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -11,7 +11,7 @@ describe("Employee", () => {
             const employee = new Employee("Jerf", "B870943", "Jerf@email.com");
 
             // expect that the arguments get passed through and create object with all three arguments
-            expect(employee.name).toEqual("Jerf");
+            expect(employee.nerm).toEqual("Jerf");
             expect(employee.id).toEqual("B870943");
             expect(employee.email).toEqual("Jerf@email.com");  
         });
@@ -44,113 +44,113 @@ describe("Employee", () => {
 // * `getEmail()`
 // * `getRole()`&mdash;returns `'Employee'`
 
-describe("Arithmetic", () => {
-  describe("Initialization", () => {
-    it("should return an object containing a 'number' property when called with the 'new' keyword", () => {
-      const obj = new Arithmetic();
+// describe("Arithmetic", () => {
+//   describe("Initialization", () => {
+//     it("should return an object containing a 'number' property when called with the 'new' keyword", () => {
+//       const obj = new Arithmetic();
 
-      expect("number" in obj).toEqual(true);
-    });
+//       expect("number" in obj).toEqual(true);
+//     });
 
-    it("should set 'number' when created", () => {
-      const num = 108;
+//     it("should set 'number' when created", () => {
+//       const num = 108;
 
-      const obj = new Arithmetic(num);
+//       const obj = new Arithmetic(num);
 
-      expect(obj.number).toEqual(num);
-    });
+//       expect(obj.number).toEqual(num);
+//     });
 
-    it("should default 'number' to 0", () => {
-      const obj = new Arithmetic();
+//     it("should default 'number' to 0", () => {
+//       const obj = new Arithmetic();
 
-      expect(obj.number).toEqual(0);
-    });
-  });
+//       expect(obj.number).toEqual(0);
+//     });
+//   });
 
-  describe("addTodo", () => {
-    // Positive Tests
-    it("should add a new 'Todo' object to its 'todos' array", () => {
-      // Arrange
-      const todoList = new TodoList();
-      const todoText = "Mow Lawn";
+//   describe("addTodo", () => {
+//     // Positive Tests
+//     it("should add a new 'Todo' object to its 'todos' array", () => {
+//       // Arrange
+//       const todoList = new TodoList();
+//       const todoText = "Mow Lawn";
 
-      // Act
-      todoList.addTodo(todoText);
+//       // Act
+//       todoList.addTodo(todoText);
 
-      // Assert
-      expect(todoList.todos.length).toEqual(1);
-      expect(todoList.todos[0] instanceof Todo).toEqual(true);
-      expect(todoList.todos[0].text).toEqual(todoText);
-    });
+//       // Assert
+//       expect(todoList.todos.length).toEqual(1);
+//       expect(todoList.todos[0] instanceof Todo).toEqual(true);
+//       expect(todoList.todos[0].text).toEqual(todoText);
+//     });
 
-    // Exception test
-    it("should throw an error if not provided text", () => {
-      // Arrange
-      const todoList = new TodoList();
-      const err = new Error(
-        "Expected parameter 'text' to be a non empty string"
-      );
-      const cb = () => todoList.addTodo();
+//     // Exception test
+//     it("should throw an error if not provided text", () => {
+//       // Arrange
+//       const todoList = new TodoList();
+//       const err = new Error(
+//         "Expected parameter 'text' to be a non empty string"
+//       );
+//       const cb = () => todoList.addTodo();
 
-      // Assert
-      expect(cb).toThrowError(err);
-    });
+//       // Assert
+//       expect(cb).toThrowError(err);
+//     });
 
-    describe("Child", () => {
-        // TODO: Add a comment describing the purpose of the following test suite
+//     describe("Child", () => {
+//         // TODO: Add a comment describing the purpose of the following test suite
       
-        describe("Initialization", () => {
-            // testing if we can create a new child object
-          it("should create an object with a name and age if provided valid arguments", () => {
-            const child = new Child("Sarah", 3);
+//         describe("Initialization", () => {
+//             // testing if we can create a new child object
+//           it("should create an object with a name and age if provided valid arguments", () => {
+//             const child = new Child("Sarah", 3);
       
-            // TODO: Add a comment describing the purpose of the following statements
-            // checking to see if the parameters we passed through are attributes of the object
-            expect(child.name).toEqual("Sarah");
-            expect(child.age).toEqual(3);
-          });
+//             // TODO: Add a comment describing the purpose of the following statements
+//             // checking to see if the parameters we passed through are attributes of the object
+//             expect(child.name).toEqual("Sarah");
+//             expect(child.age).toEqual(3);
+//           });
       
-          it("should throw an error if provided no arguments", () => {
-            // TODO: Add a comment describing the purpose of the following expression
-            // creating an empty child object
-            const cb = () => new Child();
+//           it("should throw an error if provided no arguments", () => {
+//             // TODO: Add a comment describing the purpose of the following expression
+//             // creating an empty child object
+//             const cb = () => new Child();
       
-            // TODO: Add a comment describing the purpose of the following statement
-            // checking to see if empty child object throws an error when passed through (no args!)
-            expect(cb).toThrow();
-          });
+//             // TODO: Add a comment describing the purpose of the following statement
+//             // checking to see if empty child object throws an error when passed through (no args!)
+//             expect(cb).toThrow();
+//           });
       
-          it("should throw an error if not provided an age", () => {
-            const cb = () => new Child("Sarah");
+//           it("should throw an error if not provided an age", () => {
+//             const cb = () => new Child("Sarah");
       
-            // TODO: Add a comment describing the purpose of the following declaration
-            const err = new Error("Expected parameter 'age' to be a non-negative number");
-            // has to be a positive number
+//             // TODO: Add a comment describing the purpose of the following declaration
+//             const err = new Error("Expected parameter 'age' to be a non-negative number");
+//             // has to be a positive number
       
-            // TODO: Add a comment describing the purpose of the following statement
-            expect(cb).toThrowError(err);
-            // if only name is passed though and no age, throw the error ^^ this error specifically not in general
-          });
+//             // TODO: Add a comment describing the purpose of the following statement
+//             expect(cb).toThrowError(err);
+//             // if only name is passed though and no age, throw the error ^^ this error specifically not in general
+//           });
       
-          it("should throw an error if 'name' is not a string", () => {
-            const cb = () => new Child(3, 2);
-            const err = new Error("Expected parameter 'name' to be a non-empty string");
+//           it("should throw an error if 'name' is not a string", () => {
+//             const cb = () => new Child(3, 2);
+//             const err = new Error("Expected parameter 'name' to be a non-empty string");
       
-            expect(cb).toThrowError(err);
-          }); // if err error is thrown when non-string name is passed through--> 3 is not a name
+//             expect(cb).toThrowError(err);
+//           }); // if err error is thrown when non-string name is passed through--> 3 is not a name
       
-          it("should throw an error if 'age' is not a number", () => {
-            const cb = () => new Child("Sarah", "2");
-            const err = new Error("Expected parameter 'age' to be a non-negative number");
+//           it("should throw an error if 'age' is not a number", () => {
+//             const cb = () => new Child("Sarah", "2");
+//             const err = new Error("Expected parameter 'age' to be a non-negative number");
       
-            expect(cb).toThrowError(err);
-          }); // if err error is thrown when non-number number is passed as a number
+//             expect(cb).toThrowError(err);
+//           }); // if err error is thrown when non-number number is passed as a number
       
-          it("should throw an error if 'age' is less than 0", () => {
-            const cb = () => new Child("Sarah", -1);
-            const err = new Error("Expected parameter 'age' to be a non-negative number");
+//           it("should throw an error if 'age' is less than 0", () => {
+//             const cb = () => new Child("Sarah", -1);
+//             const err = new Error("Expected parameter 'age' to be a non-negative number");
       
-            expect(cb).toThrowError(err);
-          }); // if err error is thrown when the age passed is negative
-        });
-      });
+//             expect(cb).toThrowError(err);
+//           }); // if err error is thrown when the age passed is negative
+//         });
+//       });
