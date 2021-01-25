00a26a2a9fa23e269ef969c4f666d6bd6fcf8479
J-Boogie <jackr353@gmail.com>
2021-01-25 15:18:15 -0600

    add manager Qs, think it works?

--- index.js
+++ index.js
@@ -9,23 +9,89 @@ const inquirer = require("inquirer")
 const fs = require("fs")
 
 // shared questions: 
-var sharedQs = ["What is their name?", "What is their employee ID?", "What is their email address?"]
-// specific questions
-var askOffice = "What is their office number?"
-var askGithub = "What is their github username?"
-var askSchool = "What school do they attend?"
+var sharedQs = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "Enter employee's name",
+        validate: name => {
+            if (name) {
+                return true
+            } else {
+                console.log("Please input a valid name entry")
+                return false
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Enter employee's email address",
+        validate: email => {
+            if (email && email.includes("@")) {
+                return true
+            } else {
+                console.log("Please enter a valid email address")
+                return false
+            }
+        }
+    }, 
+    {
+        type: 'input',
+        name: 'id',
+        message: "Enter employee's id",
+        validate: id => {
+            if (id) {
+                return true
+            } else {
+                console.log("Please enter a valid id")
+                return false
+            }
+        }
+    }
+]
 
 // application function
 function runApp() {
-    sharedQs.forEach(
-        inquirer
-        .prompt([{
-            type: 'input',
-            name: 'name',
-            message: sharedQs[i],
-        }])
+    inquirer
+    .prompt([
+        sharedQs[0],
+        sharedQs[1],
+        sharedQs[2],
+        {
+            type: 'list',
+            name: 'role',
+            message: "Select employee's role",
+            choices: ["Manager", "Engineer", "Intern", "Employee"],
+        },
+    ]).then(answers => {
+
+        // if manager = role, ask for office number
+        if(answers.role === 'Manager') {
+
+            inquirer.prompt([{
+                type: 'input',
+                name: 'officeNum',
+                message: "Enter Manager's office number",
+                validate: officeNum => {
+                    if (officeNum) {
+                        return true;
+                    } else {
+                        console.log(' Please enter valid office number');
+                        return false;
+                    }
+                }
+            }])
+            // create manager object
+            .then(answers => {
+                console.log(answers.officeNum)
+                const manager = new Manager(answers.name, answers.email, answers.id, answers.role, answers.officeNum )
+                console.log(manager);
+            });
+        }}
     )
 }
+
 runApp()
 
 // prompt user for team information
